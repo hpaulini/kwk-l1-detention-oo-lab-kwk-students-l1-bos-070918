312380b1f4824dbafd95e87ffdaff398f52492d2
hpaulini <helenpaulini@gmail.com>
2018-07-13 15:19:30 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -5,7 +5,7 @@ class Detention
   attr_accessor :location, :time, :grumpy_teacher, :activity
   
   def initialize (activity = "collective punishment")
-    
+    @activity = activity
   end
   
 end
